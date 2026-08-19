56a500328bde3cc9c99436ea92648bf9883f08d8
LLLLLLisaa <xianochchu@gmail.com>
2026-08-19 20:45:18 +0200

    feat: initialize frontend project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,57 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from '@/pages/login/login.component';
+import { RegisterComponent } from '@/pages/register/register.component';
+import { HomeComponent } from '@/pages/home/home.component';
+import { BooksComponent } from '@/pages/books/books.component';
+import { BookFormComponent } from '@/pages/book-form/book-form.component';
+import { QuotesComponent } from '@/pages/quotes/quotes.component';
+import { QuoteFormComponent } from '@/pages/quote-form/quote-form.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: 'books/add',
+    component: BookFormComponent
+  },
+  {
+    path: 'books/edit/:id',
+    component: BookFormComponent
+  },
+  {
+    path: 'quotes',
+    component: QuotesComponent
+  },
+  {
+    path: 'quotes/add',
+    component: QuoteFormComponent
+  },
+  {
+    path: 'quotes/edit/:id',
+    component: QuoteFormComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
