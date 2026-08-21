6a4e4dee0470cb32724d7e7c5eb0579d25de673e
LLLLLLisaa <xianochchu@gmail.com>
2026-08-21 20:20:36 +0200

    feat: add book form navigation

--- src/app/pages/book-form/book-form.component.ts
+++ src/app/pages/book-form/book-form.component.ts
@@ -1,3 +1,13 @@
-export class BookFormComponent{
-    
+import { Component } from '@angular/core';
+import { HeaderComponent } from '@/components/header/header.component';
+
+@Component({
+  selector: 'app-book-form',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './book-form.component.html',
+  styleUrl: './book-form.component.css'
+})
+export class BookFormComponent {
+
 }
