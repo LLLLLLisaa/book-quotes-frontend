c2b6e766482b7762c35e6c5f21c072e1d02da4cb
LLLLLLisaa <xianochchu@gmail.com>
2026-08-24 11:13:18 +0200

    feat: implement book editing

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,7 +3,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   const token = localStorage.getItem('token');
-  console.log('token from interceptor:', token);
 
   if (token) {
 
--- src/app/pages/book-form/book-form.component.ts
+++ src/app/pages/book-form/book-form.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute,Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BookService } from '@/services/book.service';
 import { HeaderComponent } from '@/components/header/header.component';
@@ -12,14 +12,18 @@ import { HeaderComponent } from '@/components/header/header.component';
     templateUrl: './book-form.component.html',
     styleUrls: ['./book-form.component.css']
   })
-export class BookFormComponent {
+export class BookFormComponent implements OnInit{
+    bookForm;
 
-  bookForm;
+    bookId?: number;
+    isEditMode = false;
+
 
   constructor(
     private fb: FormBuilder,
     private bookService: BookService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.bookForm = this.fb.group({
       title: ['', Validators.required],
@@ -34,18 +38,55 @@ export class BookFormComponent {
     });
   }
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+        this.isEditMode = true;
+        this.bookId = Number(id);
+
+        this.bookService.getBook(this.bookId).subscribe({
+        next: (book) => {
+            this.bookForm.patchValue({
+            title: book.title,
+            author: book.author,
+            publicationDate: book.publicationDate
+            .substring(0, 10)
+            .replaceAll('-', '.')
+            });
+        },
+        error: (err) => {
+            console.error(err);
+        }
+        });
+    }
+  }
+
   onSubmit(): void {
     if (this.bookForm.invalid) {
       this.bookForm.markAllAsTouched();
       return;
     }
-
+  
     const book = {
       title: this.bookForm.value.title!,
       author: this.bookForm.value.author!,
       publicationDate: this.bookForm.value.publicationDate!
     };
-
+  
+    if (this.isEditMode && this.bookId) {
+      this.bookService.updateBook(this.bookId, book).subscribe({
+        next: () => {
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      });
+  
+      return;
+    }
+  
     this.bookService.addBook(book).subscribe({
       next: () => {
         this.router.navigate(['/home']);
